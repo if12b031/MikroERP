44098cf72da40ee88dc8e292fcbb503ab0520aa1
if12b103 <georg.huszar@gmail.com>
2014-03-28 17:07:40 +0100

    GUI exists (not finished)

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,19 +1,21 @@
 package main;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.TabPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			TabPane root = (TabPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root, 600, 600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("SWE 2 - MikroERP");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
