2bf1527a7f1e525740f317e8b41b0894e1b58d51
if12b103 <georg.huszar@gmail.com>
2014-06-10 20:11:08 +0200

    added PMD report (codeCheck)

--- src/contacts/Customer.java
+++ src/contacts/Customer.java
@@ -8,45 +8,45 @@ public class Customer {
 	private String _title;
 	private String _suffix;
 	private String _surname;
-	private String _lastname;	
+	private String _lastname;
 	private String _dateOfBirth;
 	private String _employedAt;
 	private String _address;
 	private int _plz;
 	private String _city;
-	
+
 	public int get_id() {
 		return _id;
 	}
-	
+
 	public void set_id(int _id) {
 		this._id = _id;
 	}
-	
+
 	public String get_uid() {
 		return _uid;
 	}
-	
+
 	public void set_uid(String _uid) {
 		this._uid = _uid;
 	}
-	
+
 	public String get_name() {
 		return _name;
 	}
-	
+
 	public void set_name(String _name) {
 		this._name = _name;
 	}
-	
+
 	public String get_address() {
 		return _address;
 	}
-
+	
 	public void set_address(String _address) {
 		this._address = _address;
 	}
-
+	
 	public int get_plz() {
 		return _plz;
 	}
@@ -66,11 +66,11 @@ public class Customer {
 	public String get_surname() {
 		return _surname;
 	}
-	
+
 	public void set_surname(String _surname) {
 		this._surname = _surname;
 	}
-	
+
 	public String get_lastname() {
 		return _lastname;
 	}
@@ -78,35 +78,35 @@ public class Customer {
 	public void set_lastname(String _lastname) {
 		this._lastname = _lastname;
 	}
-	
+
 	public String get_title() {
 		return _title;
 	}
-	
+
 	public void set_title(String _title) {
 		this._title = _title;
 	}
-	
+
 	public String get_suffix() {
 		return _suffix;
 	}
-	
+
 	public void set_suffix(String _suffix) {
 		this._suffix = _suffix;
 	}
-	
+
 	public String get_dateOfBirth() {
 		return _dateOfBirth;
 	}
-	
+
 	public void set_dateOfBirth(String _dateOfBirth) {
 		this._dateOfBirth = _dateOfBirth;
 	}
-	
+
 	public String get_employedAt() {
 		return _employedAt;
 	}
-	
+
 	public void set_employedAt(String _employedAt) {
 		this._employedAt = _employedAt;
 	}
